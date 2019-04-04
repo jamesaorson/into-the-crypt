cdf755535fc21e3d118ff43acff9446f571eebfc
James Osborne <james.osborne112@gmail.com>
2019-04-04 12:13:50 -0400

    Add plane to move on and move relative to camera position

--- world/player.gd
+++ world/player.gd
@@ -1,27 +1,47 @@
-extends CSGBox
+extends KinematicBody
 
-var velocity = 10
-var forward = { vector = Vector3(0, 0, 1), inputName = "player_move_forward" }
-var backward = { vector = -forward.vector, inputName = "player_move_backward" }
-var left = { vector = Vector3(1, 0, 0), inputName = "player_move_left" }
-var right = { vector = -left.vector, inputName = "player_move_right" }
+const FORWARD = { vector = Vector3(0, 0, 1), inputName = "player_move_forward" }
+const BACKWARD = { vector = -FORWARD.vector, inputName = "player_move_backward" }
+const LEFT = { vector = Vector3(1, 0, 0), inputName = "player_move_left" }
+const RIGHT = { vector = -LEFT.vector, inputName = "player_move_right" }
+const UP = { vector = Vector3(0, 1, 0), inputName = "player_move_up" }
+const DOWN = { vector = -UP.vector, inputName = "player_move_down" }
 
-var deadZone = 0.2
+var camera
+var gravity = -9.8
+var velocity = Vector3()
+
+const SPEED = 6
+const ACCELERATION = 3
+const DE_ACCELERATION = 5
 
 func _ready():
 	set_process(true)
+	camera = get_node("../Camera").get_global_transform()
 
-func _process(delta):
-	if Input.is_action_pressed(self.forward.inputName):
-		move(self.forward, self.velocity, delta)
-	if Input.is_action_pressed(self.backward.inputName):
-		move(self.backward, self.velocity, delta)
-	if Input.is_action_pressed(self.left.inputName):
-		move(self.left, self.velocity, delta)
-	if Input.is_action_pressed(self.right.inputName):
-		move(self.right, self.velocity, delta)
+func _physics_process(delta):
+	var direction = Vector3()
+	if (Input.is_action_pressed(self.FORWARD.inputName)):
+		direction += camera.basis[1]
+	if (Input.is_action_pressed(self.BACKWARD.inputName)):
+		direction += -camera.basis[1]
+	if (Input.is_action_pressed(self.LEFT.inputName)):
+		direction += -camera.basis[0]
+	if (Input.is_action_pressed(self.RIGHT.inputName)):
+		direction += camera.basis[0]
+	direction.y = 0
+	direction = direction.normalized()
 
-func move(direction, velocity, delta, deadZone = self.deadZone):
-	var strength = Input.get_action_strength(direction.inputName)
-	if abs(strength) > deadZone:
-		translate(direction.vector * strength * velocity * delta)
+	self.velocity.y += delta * self.gravity
+	var horizontalVelocity = self.velocity
+	horizontalVelocity.y = 0
+	
+	var newPosition = direction * SPEED
+	var acceleration = DE_ACCELERATION
+
+	if (direction.dot(horizontalVelocity) > 0):
+		acceleration = ACCELERATION
+	horizontalVelocity = horizontalVelocity.linear_interpolate(newPosition, acceleration * delta)
+	self.velocity.x = horizontalVelocity.x
+	self.velocity.z = horizontalVelocity.z
+	self.velocity = move_and_slide(self.velocity, self.UP.vector)
