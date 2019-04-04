17a767427c787aa752296488765ba35c77f52d91
JamesOsborne <james.osborne112@gmail.com>
2019-04-03 21:28:08 -0400

    Add quit on pressing escape

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,5 +1,12 @@
 extends Button
 
+func _ready():
+    set_process(true)
+
+func _process(delta):
+   if Input.is_action_pressed("exit"):
+      get_tree().quit()
+
 func _on_quit_button_pressed():
 	get_tree().quit()
 
