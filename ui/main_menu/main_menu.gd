9ea4706d5e0a8a00b15cd82f1993fc4e2b3e438d
JamesOsborne <james.osborne112@gmail.com>
2019-04-03 21:28:08 -0400

    CRYPT-2: Add camera control and movement
    Added a player-centric camera and ability to move.
    Remove unnecessary 3D code and create project export presets.

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,7 +1,24 @@
 extends Button
 
+var cryptScene = preload("res://crypt/crypt.tscn")
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		start_game()
+	if Input.is_action_pressed("ui_cancel") or Input.is_action_pressed("exit"):
+		quit_game()
+
 func _on_quit_button_pressed():
-	get_tree().quit()
+	quit_game()
 
 func _on_start_button_pressed():
-	get_tree().change_scene("res://world/world.tscn")
+	start_game()
+
+func quit_game():
+	get_tree().quit()
+
+func start_game():
+	get_tree().change_scene_to(cryptScene)
