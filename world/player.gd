6643a7beb591897b66f614ee7debbc2dac68463a
JamesOsborne <james.osborne112@gmail.com>
2019-04-03 22:50:12 -0400

    Extend movement logic to consider strength of input and joy axes

--- world/player.gd
+++ world/player.gd
@@ -1,24 +1,27 @@
 extends CSGBox
 
 var velocity = 10
-var forward = { vector = Vector3(0, 0, 1), name = "forward" }
-var backward = { vector = -forward.vector, name = "backward" }
-var left = { vector = Vector3(1, 0, 0), name = "left" }
-var right = { vector = -left.vector, name = "right" }
+var forward = { vector = Vector3(0, 0, 1), inputName = "player_move_forward" }
+var backward = { vector = -forward.vector, inputName = "player_move_backward" }
+var left = { vector = Vector3(1, 0, 0), inputName = "player_move_left" }
+var right = { vector = -left.vector, inputName = "player_move_right" }
+
+var deadZone = 0.2
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	if Input.is_action_pressed("player_move_forward"):
-		move(forward, delta)
-	if Input.is_action_pressed("player_move_backward"):
-		move(backward, delta)
-	if Input.is_action_pressed("player_move_left"):
-		move(left, delta)
-	if Input.is_action_pressed("player_move_right"):
-		move(right, delta)
+	if Input.is_action_pressed(self.forward.inputName):
+		move(self.forward, self.velocity, delta)
+	if Input.is_action_pressed(self.backward.inputName):
+		move(self.backward, self.velocity, delta)
+	if Input.is_action_pressed(self.left.inputName):
+		move(self.left, self.velocity, delta)
+	if Input.is_action_pressed(self.right.inputName):
+		move(self.right, self.velocity, delta)
 
-func move(direction, delta):
-	print(direction.name)
-	translate(direction.vector * velocity * delta)
+func move(direction, velocity, delta, deadZone = self.deadZone):
+	var strength = Input.get_action_strength(direction.inputName)
+	if abs(strength) > deadZone:
+		translate(direction.vector * strength * velocity * delta)
