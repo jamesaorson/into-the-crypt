435ed507675369d5f07beced393754c89d12d357
JamesOsborne <james.osborne112@gmail.com>
2019-04-12 20:34:37 -0400

    Add player centric camera

--- crypt/door/door.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _on_Door_body_entered(body):
-	print("Entered door")
-	var crypts = get_tree().get_nodes_in_group("crypt")
-	if len(crypts) == 0:
-		print("There are no crypts...")
-	else:
-		crypts[0].create_room()
--- crypt/room/room.gd
+++ crypt/room/room.gd
@@ -1,11 +1,9 @@
 extends TileMap
 
-onready var doorScene = load("res://crypt/door/door.tscn")
 onready var playerScene = load("res://player/player.tscn")
 
-const OPEN_TILE = 0
+const FLOOR_TILE = 0
 const WALL_TILE = 1
-const DOOR_TILE = 0
 
 const ROOM_HEIGHT = 18
 const ROOM_WIDTH = 32
@@ -14,10 +12,6 @@ const PLAYER_START_POSITION = Vector2(3, 3)
 const PLAYER_HEIGHT = 3
 const PLAYER_WIDTH = 2
 
-var DOOR_TILEMAP_POSITION = Vector2(ROOM_WIDTH - 1, ROOM_HEIGHT / 2)
-const DOOR_HEIGHT = 2
-
-var doorPosition = map_to_world(DOOR_TILEMAP_POSITION)
 var playerStartPosition = map_to_world(PLAYER_START_POSITION)
 var room = []
 
@@ -32,35 +26,17 @@ func generate_room():
 			    x < PLAYER_START_POSITION.x + PLAYER_WIDTH and 
 				y >= PLAYER_START_POSITION.y and
 				y < PLAYER_START_POSITION.y + PLAYER_HEIGHT):
-				self.room[y][x] = OPEN_TILE
-			elif (x >= DOOR_TILEMAP_POSITION.x - PLAYER_WIDTH and
-				  x <= DOOR_TILEMAP_POSITION.x and
-				  y >= DOOR_TILEMAP_POSITION.y and
-				  y < DOOR_TILEMAP_POSITION.y + DOOR_HEIGHT):
-				self.room[y][x] = DOOR_TILE
+				self.room[y][x] = FLOOR_TILE
 			elif x == 0 or x == ROOM_WIDTH - 1 or y == 0 or y == ROOM_HEIGHT - 1:
 				self.room[y][x] = WALL_TILE
 			else:
 				var tileChoice = rand_range(1, 10)
 				if tileChoice > 2:
-					self.room[y][x] = OPEN_TILE
+					self.room[y][x] = FLOOR_TILE
 				else:
 					self.room[y][x] = WALL_TILE
 	draw_room()
 	create_player()
-	create_door()
-
-func create_door():
-	var door = null
-	var doorNodes = get_tree().get_nodes_in_group("door")
-	if len(doorNodes) != 0:
-		door = doorNodes[0]
-	else:
-		print("Creating door at: ", world_to_map(self.doorPosition), " ", self.doorPosition)
-		door = doorScene.instance()
-		get_tree().root.add_child(door)
-	door.position.x = self.doorPosition.x
-	door.position.y = self.doorPosition.y
 
 func create_player():
 	var player = null
