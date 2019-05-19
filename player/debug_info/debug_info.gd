3f67e529ed3326f0c114ed4b2d83fc322dcdc162
JamesOrson <james.osborne112@gmail.com>
2019-05-19 17:20:58 -0500

    CRYPT-7: Add basic village

--- player/debug_info/debug_info.gd
+++ player/debug_info/debug_info.gd
@@ -3,7 +3,8 @@ extends CanvasLayer
 onready var cryptSeedLabel = get_node("CryptSeedLabel")
 
 func _ready():
-	cryptSeedLabel.text = "Crypt Seed: " + str(crypt_globals.cryptSeed)
+	if crypt_globals.cryptSeed != null:
+		cryptSeedLabel.text = "Crypt Seed: " + str(crypt_globals.cryptSeed)
 	cryptSeedLabel.update()
 
 func update_info():
