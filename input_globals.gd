c236dcd43ea61b33782aa2f9a7052146969716e8
JamesOrson <james.osborne112@gmail.com>
2019-05-22 20:24:44 -0500

    CRYPT-8: Add buildings to village

---
+++ input_globals.gd
@@ -0,0 +1,13 @@
+extends Node
+
+export var PAUSE = "pause_0"
+export var RESET = "reset_0"
+
+export var UI_ACCEPT = "ui_accept"
+export var SPRINT = "sprint_0"
+export var PRIMARY_ATTACK = "primary_attack_0"
+
+export var UP = "player_move_up_0"
+export var DOWN = "player_move_down_0"
+export var LEFT = "player_move_left_0"
+export var RIGHT = "player_move_right_0"
