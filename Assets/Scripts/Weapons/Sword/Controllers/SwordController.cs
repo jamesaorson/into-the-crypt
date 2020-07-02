06c45a2f6ff89f9a06e90bed1c6ed3785a8e3bd1
JamesOrson <james.osborne112@gmail.com>
2020-07-02 15:45:52 -0700

    CRYPT-27: Replace high-def textures with pixel placeholders.
    Also fix the component initialization for weapon.

--- Assets/Scripts/Weapons/Sword/Controllers/SwordController.cs
+++ Assets/Scripts/Weapons/Sword/Controllers/SwordController.cs
@@ -7,18 +7,15 @@ namespace IntoTheCrypt.Weapons.Sword.Controllers
         #region Public
 
         #region Members
-        [Tooltip("Game object that will count for collision with enemies.")]
-        public GameObject Damager;
+        [Tooltip("Weapon Animator")]
+        public Animator Animator;
+        [Tooltip("Collider of the Damager game object")]
+        public BoxCollider DamagerCollider;
         #endregion
 
         #endregion
 
         #region Private
-
-        #region Members
-        private Animator _animator;
-        private BoxCollider _damagerCollider;
-        #endregion
 
         #region Member Methods
         private void HandleActiveComboAction(string clipName)
@@ -26,18 +23,18 @@ namespace IntoTheCrypt.Weapons.Sword.Controllers
             switch (clipName)
             {
                 case null:
-                    _animator.SetTrigger("StartCombo");
-                    _animator.SetInteger("CurrentCombo", 1);
+                    Animator.SetTrigger("StartCombo");
+                    Animator.SetInteger("CurrentCombo", 1);
                     break;
                 case "Combo 1":
                     break;
                 case "Post Combo 1":
-                    _animator.SetInteger("CurrentCombo", 2);
+                    Animator.SetInteger("CurrentCombo", 2);
                     break;
                 case "Combo 2":
                     break;
                 case "Post Combo 2":
-                    _animator.SetInteger("CurrentCombo", 0);
+                    Animator.SetInteger("CurrentCombo", 0);
                     break;
                 default:
                     throw new System.Exception("Invalid combo");
@@ -49,26 +46,26 @@ namespace IntoTheCrypt.Weapons.Sword.Controllers
             switch (clipName)
             {
                 case null:
-                    _damagerCollider.enabled = false;
+                    DamagerCollider.enabled = false;
                     break;
                 case "Combo 1":
-                    _damagerCollider.enabled = true;
+                    DamagerCollider.enabled = true;
                     break;
                 case "Post Combo 1":
-                    _damagerCollider.enabled = false;
+                    DamagerCollider.enabled = false;
                     // Reset combo if nothing is being pressed and no other combo has been queued
-                    int combo = _animator.GetInteger("CurrentCombo");
+                    int combo = Animator.GetInteger("CurrentCombo");
                     if (combo != 2)
                     {
-                        _animator.SetInteger("CurrentCombo", 0);
+                        Animator.SetInteger("CurrentCombo", 0);
                     }
                     break;
                 case "Combo 2":
-                    _damagerCollider.enabled = true;
+                    DamagerCollider.enabled = true;
                     break;
                 case "Post Combo 2":
-                    _damagerCollider.enabled = false;
-                    _animator.SetInteger("CurrentCombo", 0);
+                    DamagerCollider.enabled = false;
+                    Animator.SetInteger("CurrentCombo", 0);
                     break;
                 default:
                     throw new System.Exception("Invalid combo");
@@ -78,14 +75,12 @@ namespace IntoTheCrypt.Weapons.Sword.Controllers
         [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
         private void Start()
         {
-            _animator = gameObject.GetComponent<Animator>();
-            _damagerCollider = Damager.GetComponent<BoxCollider>();
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
         private void Update()
         {
-            AnimatorClipInfo[] currentClipInfo = _animator.GetCurrentAnimatorClipInfo(0);
+            AnimatorClipInfo[] currentClipInfo = Animator.GetCurrentAnimatorClipInfo(0);
             string clipName = null;
             if (currentClipInfo.Length > 0)
             {
